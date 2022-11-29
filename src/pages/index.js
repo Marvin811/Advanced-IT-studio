b1fe97df01272e16b73a9a080abedff47449c2dc
Egor Lukin <86829973+Marvin811@users.noreply.github.com>
2022-11-29 22:00:22 +0300

    feat: БЭМ блока all-services

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,19 @@
 import "./index.css";
+// show-more
+const showMore = document.querySelector('.all-services__button');
+const productsLength = document.querySelectorAll('.all-services__block-content');
+let items = 1;
+
+showMore.addEventListener('click', () => {
+    items +=1;
+
+    const array = Array.from(document.querySelector('.all-services__block-container').children);
+    const visItems = array.slice(0, items);
+
+    visItems.forEach(element => element.classList.add('is-visible'));
+
+    if (visItems.length === productsLength) {
+        showMore.style.display = 'none'
+    }
+});
+
