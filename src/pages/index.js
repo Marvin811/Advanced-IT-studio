4238e00d6452dc2bf1b16f79ff93af3c1654f57c
Egor Lukin <86829973+Marvin811@users.noreply.github.com>
2022-11-30 23:23:41 +0300

    feat: добавление allProject in js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,37 @@
 import "./index.css";
-// show-more
-const showMore = document.querySelector('.services__button');
-const productsLength = document.querySelectorAll('.services__item').length;
-let items = 3;
+// show-more all-services
+const allServices = document.querySelector('.services__button');
+const servicesLength = document.querySelectorAll('.services__item').length;
+const allProjects = document.querySelector('.project__button');
+const projectLength = document.querySelectorAll('.project__items').length;
 
-showMore.addEventListener('click', () => {
-    items += 3;
+
+let itemsAllServices = 3;
+let itemsAllProject = 4;
+
+allServices.addEventListener('click', () => {
+    itemsAllServices += 3;
     const array = Array.from(document.querySelector('.services__block').children);
-    const visItems = array.slice(0, items);
+    const visItems = array.slice(0, itemsAllServices);
 
     visItems.forEach(el => el.classList.add('is-visible'));
 
-    if (visItems.length === productsLength) {
-        showMore.style.display = 'none';
+    if (visItems.length === servicesLength) {
+        allServices.style.display = 'none';
     }
 });
+
+allProjects.addEventListener('click', () => {
+    itemsAllProject +=1;
+    const array = Array.from(document.querySelector('.project__content').children);
+    const visItems = array.slice(0, itemsAllProject);
+
+    visItems.forEach(el => el.classList.add('visible'));
+
+    if (visItems.length === projectLength) {
+        allProjects.style.display = 'none';
+    }
+});
+
+
 
