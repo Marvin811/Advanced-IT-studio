109c85084d1d089732bd308ddb35df0328d7f98b
Egor Lukin <86829973+Marvin811@users.noreply.github.com>
2022-11-30 14:40:42 +0300

    feat: удаление блока all-services

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,18 @@
 import "./index.css";
 // show-more
-const showMore = document.querySelector('.all-services__button');
-const productsLength = document.querySelectorAll('.all-services__block-content');
-let items = 1;
+const showMore = document.querySelector('.services__button');
+const productsLength = document.querySelectorAll('.services__item').length;
+let items = 3;
 
 showMore.addEventListener('click', () => {
-    items +=1;
-
-    const array = Array.from(document.querySelector('.all-services__block-container').children);
+    items += 3;
+    const array = Array.from(document.querySelector('.services__block').children);
     const visItems = array.slice(0, items);
 
-    visItems.forEach(element => element.classList.add('is-visible'));
+    visItems.forEach(el => el.classList.add('is-visible'));
 
     if (visItems.length === productsLength) {
-        showMore.style.display = 'none'
+        showMore.style.display = 'none';
     }
 });
 
