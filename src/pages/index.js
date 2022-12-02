61960204fe6e2e57bb3e0649111cb4cb60cc9407
Egor Lukin <86829973+Marvin811@users.noreply.github.com>
2022-12-02 15:41:00 +0300

    feat: burger menu

--- src/pages/index.js
+++ src/pages/index.js
@@ -35,7 +35,43 @@ allProjects.addEventListener('click', () => {
 
 
 //burger menu
+const hamb = document.querySelector("#hamb");
+const popupHeader = document.querySelector('#popupHeader');
+// const body = document.body;
 
+//Клонируем меню, чтобы задать свои стили для мобильной версии
+const menu = document.querySelector('#menu').cloneNode(1);
 
+//При клике на иконку hamb вызываем функцию hambHandle;
+hamb.addEventListener("click", hambHandler);
+
+//Выполняем действия при клике..
+function hambHandler(e) {
+    e.preventDefault();
+    //Переключение стили элементов при клике
+    popupHeader.classList.toggle('header__open');
+    hamb.classList.toggle("active");
+    renderPopup();
+}
+
+//Выполнить рендеринг элементов в наш попап
+function renderPopup() {
+    popupHeader.appendChild(menu);
+}
+
+//Код для закрытия меню при нажатии на ссылку
+const links = Array.from(menu.children);
+
+//Для каждого элемента меню при клике вызываем ф-ию
+
+links.forEach((link => {
+    link.addEventListener('click', closeOnClick)
+}))
+
+//Закрытие попапа при клике на меню
+function closeOnClick() {
+    popupHeader.classList.toggle('header__open');
+    hamb.classList.toggle("active");
+}
 
 
